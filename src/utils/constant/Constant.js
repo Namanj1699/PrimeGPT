c62ddaba9e4cce25112074747dcb4fb81568470d
Naman Jain <jainnaman1699@gmail.com>
2024-03-31 00:28:58 +0530

    Remove bearer token for security issues

--- src/utils/constant/Constant.js
+++ src/utils/constant/Constant.js
@@ -7,11 +7,4 @@ export const movieSuggestions1 =
 export const movieSuggestions2 =
   "https://m.media-amazon.com/images/G/31/AmazonVideo/2021/X-site/MLP/TVOD/TVOD_MLP_Right.jpg";
 
-export const tmdbGetOptions = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZWMzZTAxYmMwNmY3NTJiZWUxNWVhYzg3ZDA2YWQ5NCIsInN1YiI6IjY2MDQ1NjhjMTk3ZGU0MDE4NjFlMzY0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xeCdSe-0Tc7l7E3VdpOIn8X4518OPHtAfn6uThkL9fA'
-    }
-  };
 
