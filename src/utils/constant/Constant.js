2c4ba776ae96bbb99dccd9d0fafd0ae52fc5fdfd
Naman Jain <jainnaman1699@gmail.com>
2024-04-25 13:31:56 +0530

    Fix for Netlify prod issues

--- src/utils/constant/Constant.js
+++ src/utils/constant/Constant.js
@@ -13,7 +13,7 @@ export const tmdbGetOptions = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer'+ process.env.REACT_APP_TMDB_API_KEY,
+    Authorization: process.env.REACT_APP_TMDB_API_KEY,
   }
 };
 
