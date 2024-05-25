c0b7b94fb97e0fa20f998fe360a85e6267d27d67
Naman Jain <jainnaman1699@gmail.com>
2024-05-25 21:58:06 +0530

    Adding info wrt dev

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,61 @@
-import React, { useRef } from 'react'
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
 
-    const form = useRef();
-    
-    const sendEmail = (e) => {
-        e.preventDefault();
-    
-        emailjs
-          .sendForm('service_3we6ihm', 'template_gitkgdn', form.current, {
-            publicKey: 'VMXhPxlPO24jkUWCV',
-          })
-          .then(
-            () => {
-              alert("Message Sent Successfully")
-            },
-            (error) => {
-              alert(error.text)
-            },
-          );
-      };
-      
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_3we6ihm", "template_gitkgdn", form.current, {
+        publicKey: "VMXhPxlPO24jkUWCV",
+      })
+      .then(
+        () => {
+          alert("Message Sent Successfully");
+        },
+        (error) => {
+          alert(error.text);
+        }
+      );
+  };
+
   return (
-    <div className='text-white ml-80 mt-[-150px]'>
+    <div className="text-white ml-80 mt-[-175px]">
+      <p className="text-red-600 m-4">
+        {" "}
+        Note : This functionality 'support' is currently under development, If you face any issue
+        then please wait for some time. It will be fix soon. Thanku :)
+      </p>
       <form ref={form} onSubmit={sendEmail}>
-      
-      <label className='m-72'>Name
-      <input className='mb-8 ml-10 text-black' type="text" name="from_name" />
-      </label>
-      <br/>
-      <label className='m-72'>Email
-      <input className='mb-8 ml-11 text-black' type="email" name="user_email" />
-      </label>
-      <br/>
-      <label className='m-72'>Message
-      <input className='mb-8 ml-4 text-black' type="text" name="message" />
-      </label>
-      <br/>
-      <input className='ml-[410px]' type="submit" value="Send" />
-      
-    </form>
+        <label className="m-72">
+          Name
+          <input
+            className="mb-8 ml-10 text-black"
+            type="text"
+            name="from_name"
+          />
+        </label>
+        <br />
+        <label className="m-72">
+          Email
+          <input
+            className="mb-8 ml-11 text-black"
+            type="email"
+            name="user_email"
+          />
+        </label>
+        <br />
+        <label className="m-72">
+          Message
+          <input className="mb-8 ml-4 text-black" type="text" name="message" />
+        </label>
+        <br />
+        <input className="ml-[410px]" type="submit" value="Send" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
